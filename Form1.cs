8cf826583e94bb1ce4a2ecddff9070703464505d
MN2ZXC <86985114+MN2ZXC@users.noreply.github.com>
2024-11-10 22:01:44 +0200

    Initialized form1 as well as Datagridview, added columns and rows to datagridview

--- Form1.cs
+++ Form1.cs
@@ -12,17 +12,31 @@ using System.IO;
 
 namespace StudentManagementSystem
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	  public partial class Form1 : Form
+	  {
+		    // The DataHandler being used for student data
+		    private readonly DataHandler studentDataHandler = new DataHandler();
 
+		    public Form1()
+		    {
+			      InitializeComponent(); // Form component Initialization 
+			      InitializeDataGridView(); // DataGridView to display student data
+		    }
 
+		    private void InitializeDataGridView()
+		    {
+			      // Clear existing columns if there are any
+			      dataGridView1.Columns.Clear();
 
+			      // Defines the number of columns and their full names in the DataGridView
+			      dataGridView1.ColumnCount = 4;
+			      dataGridView1.Columns[0].Name = "Student ID";
+			      dataGridView1.Columns[1].Name = "Name";
+			      dataGridView1.Columns[2].Name = "Age";
+			      dataGridView1.Columns[3].Name = "Course";
+		    }
 
-        private void AddStudent_Click(object sender, EventArgs e)
+		private void AddStudent_Click(object sender, EventArgs e)
         {
 
         }
